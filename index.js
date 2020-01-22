6d3ef814306610eb5f8a7a47f1f305eb5e582afd
Ivan Pastukhov <spirithntr@gmail.com>
2020-01-23 00:11:51 +0400

    refactor async code, add pug

--- index.js
+++ index.js
@@ -3,24 +3,30 @@ const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
 const credentials = require('./credentials.json');
 
-MongoClient.connect(
-  `mongodb://${credentials.login}:${credentials.password}@${credentials.address}.mlab.com:${credentials.port}/${credentials.dbname}`
-  , (err, client) => {
-    if (err) {
-      console.log(err);
-      return;
-    }
-    let db = client.db(credentials.dbname);
-    db.collection('reviews').find().toArray(function (err, results) {
-      console.log(results)
-      // send HTML file populated with quotes here
-    })
-    db.collection('reviews');
+const dbUrl = `mongodb://${credentials.login}:${credentials.password}@${credentials.address}.mlab.com:${credentials.port}/${credentials.dbname}`;
+
+let collection;
+
+MongoClient.connect(dbUrl)
+  .then(client => client.db(credentials.dbname))
+  .then(db => db.collection('reviews'))
+  .then(coll => {
+    collection = coll;
+    return collection.find().toArray();
+  })
+  .then(result => console.log(result))
+  .then(() => init());
+
+function init() {
   const app = express();
 
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(express.static("public"));
-  app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'))
+  app.set('view engine', 'pug');
+  app.use(
+    "/css",
+    express.static(__dirname + "/node_modules/bootstrap/dist/css")
+  );
 
   app.listen(3000, function() {
     console.log("listening on 3000");
@@ -31,8 +37,11 @@ MongoClient.connect(
   });
 
   app.post("/review", (req, res) => {
-    console.log(req.body);
-    db.collection('reviews').insertOne(req.body);
+    collection.insertOne(req.body);
     res.redirect("/");
   });
-});
+
+  app.get('/test', function(req, res) {
+    res.render("test", { title: "Hey", message: "Hello there!" });
+  });
+}
