10b67b700fd62c5c872185cc0deb3748546d21c3
Ivan Pastukhov <spirithntr@gmail.com>
2020-01-25 20:52:36 +0400

    add linter, prettier remove comment

--- stat-analyzer.js
+++ stat-analyzer.js
@@ -1,33 +1,34 @@
 function analyzer(data) {
   const sumNCount = data.reduce(averageReducer, {
-    overall: [0,0],
-    service: [0,0],
-    price: [0,0],
-    product: [0,0],
-    share: [0,0]
+    overall: [0, 0],
+    service: [0, 0],
+    price: [0, 0],
+    product: [0, 0],
+    share: [0, 0]
   });
-  for (prop in sumNCount) {
-    const [sum, count] = sumNCount[prop];
+  // eslint-disable-next-line array-callback-return
+  Object.keys(sumNCount).map(criterion => {
+    const [sum, count] = sumNCount[criterion];
     if (count > 0) {
-      sumNCount[prop] = [sum / count, count];
-    };
-  }
+      sumNCount[criterion] = [sum / count, count];
+    }
+  });
   const comments = data.reduce((acc, cur) => {
     if (cur.quote) {
       acc.push(cur.quote);
-    };
+    }
     return acc;
   }, []);
   return { comments, stats: sumNCount };
 }
 
 function averageReducer(acc, current) {
-  ['overall', 'service', 'price', 'product', 'share'].forEach((prop) => {
+  ['overall', 'service', 'price', 'product', 'share'].forEach(prop => {
     if (current[prop]) {
       acc[prop][0] += Number(current[prop]);
-      acc[prop][1]++;
+      acc[prop][1] += 1;
     }
-  })
+  });
   return acc;
 }
 
